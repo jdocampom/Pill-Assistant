ecad7d81f68503f3a688358cc47b78f748ecfa0f
jdocampom <gummy_pond0v@icloud.com>
2022-03-19 15:44:54 -0500

    Initial commit

---
+++ Pill Assistant/PillAssistantApp.swift
@@ -0,0 +1,19 @@
+//
+//  Pill_AssistantApp.swift
+//  Pill Assistant
+//
+//  Created by Juan Diego Ocampo on 19/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PillAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            ContentView()
+            
+        }
+    }
+}
